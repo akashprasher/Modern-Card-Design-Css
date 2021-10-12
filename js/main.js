4eec3f24e7b425eb88e4551daa91004c9c8209e3
Akash Prasher <prince.akash.prasher@gmail.com>
2021-10-12 22:30:00 +0530

    adding message
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var warning = document.querySelector(".warning");
+var warningp = document.querySelector(".warning-p");
+
+window.onload = function () {
+  console.log(warning);
+  warning.classList.add("brdr-1");
+  warningp.classList.add("brdr-2");
+};
